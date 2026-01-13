305d88a252964f3d76e52df5a8d6860f0c5c7aa2
Alina Numanova <alinanumanova@Air-Alina.lan>
2026-01-13 17:55:23 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+//темная тема
+const toggle = document.getElementById("toggle-theme");
+const html = document.documentElement;
+
+toggle.onclick = () => {
+  const isDark = html.getAttribute("data-theme") === "dark";
+  html.setAttribute("data-theme", isDark ? "light" : "dark");
+};
+//ховеры на чат
+const chatItems = document.querySelectorAll(".chat-item, .contact-item");
+
+chatItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    chatItems.forEach((el) => el.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+// модалка
+const modalWindow = document.getElementById("modal-window");
+const openModal = document.getElementById("open-modal");
+const closeModal = document.getElementById("close-modal");
+
+openModal.addEventListener("click", () => {
+  modalWindow.classList.add("active");
+});
+
+closeModal.addEventListener("click", () => {
+  modalWindow.classList.remove("active");
+});
+modalWindow.addEventListener("click", (e) => {
+  if (e.target === modalWindow) {
+    modalWindow.classList.remove("active");
+  }
+});
+// Профиль (drawer)
+const openBtn = document.getElementById("openProfile");
+const drawer = document.getElementById("profileDrawer");
+const overlay = document.getElementById("overlay");
+const closeBtn = document.getElementById("close-profile");
+function openDrawer() {
+  drawer.classList.add("is-open");
+  overlay.classList.add("is-open");
+  document.body.classList.add("no-scroll");
+  drawer.setAttribute("aria-hidden", "false");
+}
+
+function closeDrawer() {
+  drawer.classList.remove("is-open");
+  overlay.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
+  drawer.setAttribute("aria-hidden", "true");
+}
+
+openBtn.addEventListener("click", openDrawer);
+closeBtn.addEventListener("click", closeDrawer);
+overlay.addEventListener("click", closeDrawer);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeDrawer();
+});
+
+//side nav
+const openSideBarBtn = document.getElementById("side-nav");
+const sideBar = document.querySelector(".chat-bar");
+const closeSideBarBtn = document.getElementById("close-side-bar");
+openSideBarBtn.addEventListener("click", () => {
+  sideBar.classList.add("is-open");
+
+  document.body.classList.add("no-scroll");
+});
+overlay.addEventListener("click", () => {
+  sideBar.classList.remove("is-open");
+
+  document.body.classList.remove("no-scroll");
+});
+closeSideBarBtn.addEventListener("click", () => {
+  sideBar.classList.remove("is-open");
+
+  document.body.classList.remove("no-scroll");
+});
+openSideBarBtn.addEventListener("click", openSideBar);
